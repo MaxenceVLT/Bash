c7e2443697adfcbf6ccfa22e5680042bb9de3e96
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-05-23 13:15:43 +0200

    update the system

--- upgrade-system.sh
+++ upgrade-system.sh
@@ -3,3 +3,16 @@
 aptitude update
 aptitude safe-upgrade -y
 aptitude purge && aptitude autoclean
+echo
+echo "===[Les packets ont   t   mis    jour et le cache nettoy  .]==="
+echo
+sleep 2
+
+while true; do
+    read -p "Voulez-vous mettre    jour le syst  me entier ? O/N " choice
+    case $choice in
+        [Oo]* ) echo "===[Le syst  me va etre mit    jour.]===" && aptitude full-upgrade && echo "===[Le systeme a   t   mit    jour]===" && exit;;
+        [Nn]* ) echo "===[Vous avez choisi de ne pas mettre    jour le syst  me.]===" && exit;;
+            * ) echo "===[Veuillez choisir Oui ou Non.]===";;
+    esac
+done
