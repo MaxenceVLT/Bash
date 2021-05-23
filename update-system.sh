250aa2eaa6b46a9d226b74068b54338a6b7aa74c
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-05-24 01:01:14 +0200

    update-system

--- update-system.sh
+++ update-system.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+set meta-flag on && set convert-meta off && set output-meta on #[Permet de mettre les accents]
+
+
 aptitude update
 aptitude safe-upgrade -y
 aptitude purge && aptitude autoclean
+
 echo
-echo "===[Les packets ont   t   mis    jour et le cache nettoy  .]==="
+echo "===[Les packets ont été mis à jour et le cache nettoyé.]==="
 echo
+
 sleep 2
 
 while true; do
-    read -p "Voulez-vous mettre    jour le syst  me entier ? O/N " choice
+    read -p "Voulez-vous mettre à jour le système entier ? O/N " choice
     case $choice in
-        [Oo]* ) echo "===[Le syst  me va etre mit    jour.]===" && aptitude full-upgrade && echo "===[Le systeme a   t   mit    jour]===" && exit;;
-        [Nn]* ) echo "===[Vous avez choisi de ne pas mettre    jour le syst  me.]===" && exit;;
+        [Oo]* ) echo "===[Le système va etre mit à jour.]===" && aptitude full-upgrade && echo "===[Le systeme a été mit à jour]===" && exit;;
+        [Nn]* ) echo "===[Vous avez choisi de ne pas mettre à jour le système.]===" && exit;;
             * ) echo "===[Veuillez choisir Oui ou Non.]===";;
     esac
 done
