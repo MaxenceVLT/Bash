8b268bbb2f07a8c65fb236c490baa20c2ca212f0
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-05-23 13:19:57 +0200

    Update update-system.sh

--- update-system.sh
+++ update-system.sh
@@ -9,10 +9,10 @@ echo
 sleep 2
 
 while true; do
-    read -p "Voulez-vous mettre    jour le syst  me entier ? O/N " choice
+    read -p "Voulez-vous mettre à jour le système entier ? O/N " choice
     case $choice in
-        [Oo]* ) echo "===[Le syst  me va etre mit    jour.]===" && aptitude full-upgrade && echo "===[Le systeme a   t   mit    jour]===" && exit;;
-        [Nn]* ) echo "===[Vous avez choisi de ne pas mettre    jour le syst  me.]===" && exit;;
+        [Oo]* ) echo "===[Le système va etre mit à jour.]===" && aptitude full-upgrade && echo "===[Le systeme a été mit à jour]===" && exit;;
+        [Nn]* ) echo "===[Vous avez choisi de ne pas mettre à jour le système.]===" && exit;;
             * ) echo "===[Veuillez choisir Oui ou Non.]===";;
     esac
 done
