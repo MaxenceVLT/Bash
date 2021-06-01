b15628692febdee5db95b32000a1b51eb6db7875
MaxenceVLT <84088029+MaxenceVLT@users.noreply.github.com>
2021-06-01 17:01:10 +0200

    automate-setup

---
+++ automate-setup.sh
@@ -0,0 +1,4 @@
+apt-get update && apt-get install aptitude
+aptitude update
+aptitude install thunderbird libreoffice keepassx visual-studio-code firefox-esr firefox-esr-l10n-fr openssh-client filezilla
+aptitude full-upgrade
